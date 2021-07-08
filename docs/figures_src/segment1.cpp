2848f0bd52c09f221bdf507846f8a33c91ed04d9
S. Kramm <sebastien.kramm@univ-rouen.fr>
2021-07-08 17:48:21 +0200

    added doc figures

---
+++ docs/figures_src/segment1.cpp
@@ -0,0 +1,18 @@
+#define HOMOG2D_USE_OPENCV
+
+#include "homog2d.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace homog2d;
+int main()
+{
+	Segment s1(50,50, 250,50 );
+	Segment s2(150,150, 150,50 );
+
+	cv::Mat img;
+	img.create( 350, 450, CV_8UC3 );
+	img = cv::Scalar(255,255,255);
+	s1.draw( img, CvDrawParams().setColor(250,0,20) );
+	s2.draw( img, CvDrawParams().setColor(20,0,250) );
+	cv::imwrite( "segment1.png", img );
+}
