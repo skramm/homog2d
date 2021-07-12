0c1099fe821ea264110d0bfe30a7319e9659e94d
skramm <sebastien.kramm@univ-rouen.fr>
2021-07-12 10:29:56 +0200

    minor

---
+++ misc/no_build/no_build_8.cxx
@@ -0,0 +1,6 @@
+// no intersection between a point and a polyline
+
+Polyline pl;
+Point2d pt;
+auto i = pt.intersects( pl );
+
---
+++ misc/no_build/no_build_9.cxx
@@ -0,0 +1,6 @@
+// homography * FRecct does NOT produce a FRect !
+
+FRect rect;
+Homogr H;
+FRect rect2 = H * rect;
+
