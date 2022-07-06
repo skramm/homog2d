4e6590d3133f47c344f174c460c3d6108fcd9c62
skramm <sebastien.kramm@univ-rouen.fr>
2022-07-06 23:58:19 +0200

    wip: point inside polyline

--- misc/no_build/no_build_18.cxx
+++
@@ -1,4 +0,0 @@
-// a point cannot be inside a line
-	Line2d li;
-	Point2d pt;
-	auto b = pt.isInside(li);
--- misc/no_build/no_build_19.cxx
+++
@@ -1,4 +0,0 @@
-// a point cannot be inside a segment
-	Segment seg;
-	Point2d pt;
-	auto b = pt.isInside(seg);
