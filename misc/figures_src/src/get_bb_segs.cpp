32439c79cbf5169e8b1d2956388c4a6c4de9d924
S. Kramm <sebastien.kramm@univ-rouen.fr>
2022-11-10 23:26:59 +0100

    expand getBB on containers'

--- misc/figures_src/src/get_bb_segs.cpp
+++ misc/figures_src/src/get_bb_segs.cpp
@@ -6,6 +6,7 @@
 
 img::Image<cv::Mat>       img1;
 img::Image<img::SvgImage> img2;
+std::vector<img::Color>   v_colors;
 
 std::vector<img::Color> genRandomColors( size_t nb )
 {
@@ -26,10 +27,9 @@ std::vector<img::Color> genRandomColors( size_t nb )
 template<typename T>
 void process( const T& vec, std::string fname )
 {
-	auto vcol = genRandomColors( vec.size() );
-	auto lamb_func = [&](int i)   // lambda, needed to fetch color from index
+	auto lamb_func = [v_colors](int i)   // lambda, needed to fetch color from index
 	{
-		return img::DrawParams().setColor( vcol[i] );
+		return img::DrawParams().setColor( v_colors[i] );
 	};
 	std::function<img::DrawParams(int)> f(lamb_func);
 
@@ -37,6 +37,7 @@ void process( const T& vec, std::string fname )
 	img2.clear();
 	draw( img1, vec, f );
 	draw( img2, vec, f );
+
 	auto bb = getBB( vec );
 	bb.draw( img1, DrawParams().setColor(250,50,20) );
 	bb.draw( img2, DrawParams().setColor(250,50,20) );
@@ -49,42 +50,59 @@ int main()
 {
 	std::vector<Segment> vsegs;
 	std::vector<Point2d> vpts;
+	std::vector<Point2d>   v_polypts;
+	std::vector<CPolyline> v_poly;
+	auto nb_pts_poly_max = 5;
+	auto nb_pts_poly_min = 2;
+	size_t polySize = 1.*std::rand()/RAND_MAX * nb_pts_poly_max + nb_pts_poly_min;
+
 	int xmax = 300;
 	int xmin = 40;
 	int seg_max = 50;
 	int seg_min = 10;
 	srand( time(0) );
 	int nbSegs = 60;
+
+	v_colors = genRandomColors( 2 * nbSegs );
+
 	for( int i=0; i<nbSegs; i++ )
 	{
 		auto x1 = 1.*std::rand()/RAND_MAX * xmax + xmin;
 		auto y1 = 1.*std::rand()/RAND_MAX * xmax + xmin;
 		vpts.emplace_back( Point2d(x1,y1) );
+
 		auto x2 = x1 + ((2.*std::rand()/RAND_MAX)-1.) * seg_max + seg_min;
 		auto y2 = y1 + ((2.*std::rand()/RAND_MAX)-1.) * seg_max + seg_min;
 		vpts.emplace_back( Point2d(x2,y2) );
 		vsegs.emplace_back( Segment( x1,y1,x2,y2) );
-	}
-	auto k = 1.4;
 
-//	img::Image<cv::Mat>       img1.setSize( k*xmax, k*xmax );
-//	img::Image<img::SvgImage> img2.setSize( k*xmax, k*xmax );
+		v_polypts.emplace_back( Point2d(x1,y1) );
+		if( v_polypts.size() == polySize )
+		{
+			v_poly.emplace_back( CPolyline(v_polypts) );
+			polySize = 1.*std::rand()/RAND_MAX * nb_pts_poly_max + nb_pts_poly_min;
+			v_polypts.clear();
+		}
+	}
+	auto k = 1.5;
+
 	img1.setSize( k*xmax, k*xmax );
 	img2.setSize( k*xmax, k*xmax );
 
-	process( vpts,  "bb_Points" );
-	process( vsegs, "bb_Segs" );
 
 	std::vector<FRect>   vrects;
 	for( const auto& seg: vsegs )
 		vrects.emplace_back( seg.getPts() );
 	process( vrects, "bb_Rects" );
 
-	std::vector<FRect> vcircles;
+	std::vector<Circle> vcircles;
 	for( const auto& seg: vsegs )
 	{
 		auto ppts = seg.getPts();
 		vcircles.emplace_back( ppts.first, ppts.second );
 	}
+	process( vpts,     "bb_Points" );
+	process( vsegs,    "bb_Segs" );
 	process( vcircles, "bb_Circles" );
+	process( v_poly,   "bb_Poly" );
 }
