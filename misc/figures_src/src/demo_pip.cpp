69c5d376a514510dd84151b4907e1ba286d5fd25
S. Kramm <sebastien.kramm@univ-rouen.fr>
2024-03-16 10:29:59 +0100

    wip

--- misc/figures_src/src/demo_pip.cpp
+++ misc/figures_src/src/demo_pip.cpp
@@ -1,6 +1,6 @@
 /**
 \file
-\brief Demo of point inside polygon algorithm
+\brief Demo of point inside polygon (PIP) algorithm
 */
 #define HOMOG2D_USE_OPENCV
 //#define HOMOG2D_DEBUGMODE
@@ -9,17 +9,23 @@
 using namespace h2d;
 
 template<typename P,typename BB, typename PT, typename IMG>
-void drawBase( const P& poly, const BB& bb, const PT& pt, IMG& im )
+void drawBase( const P& poly, const BB& bb, const PT& pt, const Segment* seg, IMG& im )
 {
 	im.clear();
 	poly.draw( im, DrawParams().setColor(250,0,20)  );
 	bb.draw( im, DrawParams().setColor(0,0,250)  );
 	pt.draw( im, DrawParams().setColor(0,250,0)  );
+	if( seg )
+	{
+		seg->draw( im, DrawParams().setColor(0,0,250)  );
+		auto inter = seg->intersects( poly );
+		inter.get()[0].draw( im, DrawParams().setColor(0,250,0).setPointStyle(PtStyle::Dot).setWidth(3) );
+	}
 }
 
 int main()
 {
-	auto nbim = 20; // nb images
+//	auto nbim = 20; // nb images
 
 	std::vector<Point2d> vpts1{
 		{ 0, 0 },
@@ -35,43 +41,53 @@ int main()
 
 		{ 0, 10 }
 	};
-
-	Point2d pt(5,5);
-
 	CPolyline pol( vpts1 );
 
-	auto H = Homogr().addTranslation(10,10).addScale(8);
+
+// considered point is exactly in the center of the polygon
+	Point2d pt(5,5);
+
+	auto H = Homogr().addTranslation(20,20).addScale(8);
 	auto poly = H*pol;
 	auto pt0 = H * pt;
-	auto bb = getBB( poly );
-	auto bbe = bb.getExtended();
+	auto bbe = getBB( poly ).getExtended();
 
-	img::Image<img::SvgImage> im( 350,250 );
+	img::Image<img::SvgImage> im( 350,350 );
 
-	drawBase( poly, bbe, pt0, im );
+	drawBase( poly, bbe, pt0, nullptr, im );
 
 	im.write( "demo_pip_0.svg" );
+	auto pts = bbe.get4Pts();
+
+// step 1: show that taking segments based on the 4 points
+// of the extended BB does not meet the requirements
+// (the segments will intersect the polygon on one of its points)
 	for( int i=0; i<4; i++ )
 	{
-		drawBase( poly, bbe, pt0, im );
-		auto seg = Segment( bbe.get4Pts()[i], pt0 );
-		seg.draw( im, DrawParams().setColor(0,0,250)  );
+		auto seg = Segment( pts[i], pt0 );
+		drawBase( poly, bbe, pt0, &seg, im );
+
 		std::ostringstream oss;
-		oss << "demo_pip_" << i << ".svg";
+		oss << "demo_pip_" << i+1 << ".svg";
 		im.write( oss.str() );
 	}
 
+// step 2: show that taking segments based on the middle point of the
+// 4 segments of the extended BB does not work either
 	auto segs = bbe.getSegs();
 	for( int i=0; i<4; i++ )
 	{
-		drawBase( poly, bbe, pt0, im );
 		auto ptmid = segs[i].getCenter();
 		auto seg = Segment( ptmid,  pt0 );
-		seg.draw( im, DrawParams().setColor(0,0,250)  );
+		drawBase( poly, bbe, pt0, &seg, im );
+
 		std::ostringstream oss;
-		oss << "demo_pip_" << i+4 << ".svg";
+		oss << "demo_pip_" << i+5 << ".svg";
 		im.write( oss.str() );
 	}
+
+// final step: taking the middle point of half the above segments does
+// generate a segment that does not intersect the polygon on one of its points
 
 }
 
