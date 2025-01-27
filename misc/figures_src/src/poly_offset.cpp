60047cdaac3d963f46bc88a9fd75bdbec0d71d69
skramm <sebastien.kramm@univ-rouen.fr>
2025-01-27 14:19:22 +0100

    wip: polygon offsetting (preliminar)

--- misc/figures_src/src/poly_offset.cpp
+++ misc/figures_src/src/poly_offset.cpp
@@ -5,20 +5,113 @@
 
 #include "fig_src.header"
 
+// https://stackoverflow.com/a/4609795/
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
 
+template<typename FPT>
+struct Vector
+{
+	FPT dx,dy;
+	Vector( FPT x1, FPT y1, FPT x2, FPT y2 ): dx(x2-x1), dy(y2-y1)
+	{}
+
+};
+
+template<typename FPT>
+FPT
+crossProd( const Vector<FPT>& v1, const Vector<FPT>& v2 )
+{
+	return v1.dx * v2.dy - v1.dy * v2.dx;
+}
 
 int main()
 {
-	std::vector<Point2d> vpts1{ {1,1},{3,4},{1,3} };
+//	std::vector<Point2d> vpts1{ {0,0},{5,1},{3,4} };
+	std::vector<Point2d> vpts1{ {0,0},{7,0.5},{6,4},{4,2},{1,6} };
+	std::vector<Point2d> vpt(4);
 
-	CPolyline pol(vpts1);
+	CPolyline pol0(vpts1);
 
-	auto H = Homogr().setScale(30).addTranslation(50,30);
-	img::Image<img::SvgImage> im( 550,250 );
+	auto H = Homogr().setScale(40).addTranslation(120,80);
 
-	auto pol2 = H*pol;
-	draw( im, pol2 );
-	std::cout << "main: pol2=" << pol2 << '\n';
-	auto poff = pol2.buildOffset( 30 );
-	im.write( "poly_offset.svg" );
+	auto pol = H*pol0;
+	std::cout << "main: pol2=" << pol << '\n';
+//	auto poff = pol2.buildOffset( 30 );
+	auto dist = 30;
+	size_t current = 0;
+	const auto segs = pol.getSegs();
+	const auto vppts = pol.getPts();
+	do
+	{
+		img::Image<img::SvgImage> im( 800,500 );
+		draw( im, pol );
+		std::cout << "* current=" << current << '\n';
+
+		auto nextS = (current==pol.size()-1 ? 0 : current+1);
+		auto nextPt1 = nextS;
+		auto nextPt2 = (nextPt1==pol.size()-1 ? 0 : nextPt1+1);
+
+		auto pt1 = pol.getPoint(current);
+		auto pt2 = pol.getPoint(nextPt1);
+		auto pt3 = pol.getPoint(nextPt2);
+
+		Vector<double> v1( pt1.getX(), pt1.getY(), pt2.getX(), pt2.getY() );
+		Vector<double> v2( pt2.getX(), pt2.getY(), pt3.getX(), pt3.getY() );
+
+		std::cout << "s(cp)=" << sgn(crossProd(v1,v2)) << '\n';
+//		auto v1 = std::make_pair( pol.get)
+
+//		std::cout << "current=" << current << " next=" << nextS << " nextPt2=" << nextPt2 << '\n';
+/*		auto s1 = segs.at(current);
+		auto s2 = segs.at(next);
+		std::cout << "s1=" << s1 << " s2=" << s2 << '\n';*/
+
+		draw( im, segs.at(current), DrawParams().setColor(250, 0,  0) );
+		draw( im, segs.at(nextS),    DrawParams().setColor(  0, 0,250) );
+
+
+
+		auto li1 = segs.at(current).getLine();
+		auto li2 = segs.at(nextS).getLine();
+//		std::cout << "li1=" << li1 << " li2=" << li2 << '\n';
+		std::cout << "s(li1)=" << sgn(li1.get()[1]) << " s(li2)=" <<  sgn(li2.get()[1]) << '\n';
+
+		auto pli1 = li1.getParallelLines( dist );
+		auto pli2 = li2.getParallelLines( dist );
+
+		draw( im, pli1.first,  DrawParams().setColor(250,100,  0) );
+		draw( im, pli1.second, DrawParams().setColor(250,  0,100) );
+		draw( im, pli2.first,  DrawParams().setColor(  0,100,250) );
+		draw( im, pli2.second, DrawParams().setColor(100,  0,250) );
+
+// compute the 4 intersection points
+		vpt[0] = pli1.first  * pli2.first;
+		vpt[1] = pli1.first  * pli2.second;
+		vpt[2] = pli1.second * pli2.first;
+		vpt[3] = pli1.second * pli2.second;
+
+		for( int i=0; i<4; i++ )
+		{
+			auto pt = vpt[i];
+			std::cout << "i=" << i << " pt=" << pt << " s1=" << std::to_string(side( pt, li1)) << " s2=" << side( pt, li2) << '\n';
+/*			s[i] = std::make_pair(
+				side( pt, li1 ),
+				side( pt, li2 )
+			);*/
+			auto str = std::to_string(side( pt, li1)) + ":" + std::to_string(side( pt, li2 ));
+			drawText( im, str, pt, DrawParams().setColor(0,0,0) );
+/*			drawText( im, std::to_string(side( pt, li1)), pt, DrawParams().setColor(250,0,0) );
+			drawText( im, std::to_string(side( pt, li2)), pt, DrawParams().setColor(0,250,0) );
+*/
+			im.write( "poly_offset_"+std::to_string(current) + ".svg" );
+		}
+		current++;
+	}
+	while( current<pol.size() );
+
+
+
+
 }
