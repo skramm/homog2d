de56da55e1260d287ee597302c8db2f7626c4cdd
skramm <sebastien.kramm@univ-rouen.fr>
2025-03-01 00:42:42 +0100

    test-fig target: moved grid drawing

--- misc/figures_test/t_footer_polyline_1.cxx
+++ misc/figures_test/t_footer_polyline_1.cxx
@@ -4,29 +4,7 @@
 	Homogr H;
 	H.addScale( 50 );
 	H.addTranslation( 20,20 );
-
-// grid
-	int g=220, g0=g/2;
-	auto colgrid  = img::DrawParams().setColor(g,g,g);
-	auto colgrid0 = img::DrawParams().setColor(g0,g0,g0).setThickness(2);
-	for (int i=0; i<6; i++ )
-	{
-		Line2d li( LineDir::H, i );
-		auto li2 = H*li;
-		if( i==0 )
-			li2.draw( img2, colgrid0 );
-		else
-			li2.draw( img2, colgrid );
-	}
-	for (int i=0; i<7; i++ )
-	{
-		Line2d li( LineDir::V, i );
-		auto li2 = H*li;
-		if( i==0 )
-			li2.draw( img2, colgrid0 );
-		else
-			li2.draw( img2, colgrid );
-	}
+	drawGrid( img2, H );
 
 	auto poly1 = H*pl;
 	auto dp = img::DrawParams().setColor(250,0,20).showPoints(true).setPointStyle(img::PtStyle::Times).showIndex(true);
